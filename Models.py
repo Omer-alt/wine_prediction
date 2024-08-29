1c04f950ee4a91dbdf57ce50c9b9d86f3eec6ace
Omer-alt <wafomer912@gmail.com>
2024-08-29 13:12:19 +0000

    Initial commit

---
+++ Models.py
@@ -0,0 +1,94 @@
+# For data manipulation
+import pandas as pd
+
+
+# For hypothesis testing
+from scipy.stats import ttest_1samp
+
+# For displaying all of the columns in dataframes
+pd.set_option('display.max_columns', None)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from imblearn.over_sampling import SMOTE
+
+from sklearn.model_selection import train_test_split
+
+from xgboost import XGBClassifier
+from xgboost import XGBClassifier
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load data
+df = pd.read_csv("winequality-red.csv")
+
+# Mapping from 3-8 to 0-5
+mapping = {3: 0, 4: 1, 5: 2, 6: 3, 7: 4, 8: 5}
+# Apply the mapping to the 'target' column
+df['quality'] = df['quality'].map(mapping)
+
+# Drop duplicates and save resulting dataframe in a new variable as needed
+df = df.drop_duplicates(keep='first')
+
+# Calculate Q1 (25th percentile) and Q3 (75th percentile)
+Q1 = df['pH'].quantile(0.25)
+Q3 = df['pH'].quantile(0.75)
+
+# Calculate the IQR (Interquartile Range)
+IQR = Q3 - Q1
+
+# Define the acceptable range (fence)
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# Filter out the outliers
+df_filtered = df[(df['pH'] >= lower_bound) & (df['pH'] <= upper_bound)]
+
+df = df_filtered
+
+# features (X) and target (y)
+X = df.drop('quality', axis=1)
+y = df['quality']
+
+# Feature Scaling
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(df.drop('quality', axis=1))
+
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42, stratify=y)
+
+# Create the SMOTE object
+smote = SMOTE(
+    sampling_strategy='auto',  # Strategy to sample the minority class. 'auto' means resample all minority classes.
+    random_state=42,           # Seed for random number generation.
+    k_neighbors=5,             # Number of nearest neighbors to use for generating synthetic samples.
+    n_jobs=None                # Number of CPU cores to use (-1 means use all available cores).
+)
+
+# Apply SMOTE
+X_resampled, y_resampled = smote.fit_resample(X_scaled, y)
+
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42,  stratify=y_resampled)
+
+
+
+# Let's use the model that performs best for us
+def prediction(X_test):
+    model = XGBClassifier(num_class=6, use_label_encoder=False, eval_metric='mlogloss')
+    xgb_cv = model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    
+    right_key = None
+    for key, val in mapping.items():
+        print("Val", val)
+        if val == y_pred[0]:
+            right_key = key
+            print("Prediction", y_pred[0], right_key)
+            break
+        
+        
+    return right_key
+    
---
+++ wine_streamlit.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import numpy as np
+
+from Models import prediction
+
+# Set the page configuration
+st.set_page_config(page_title="RED WINE QUALITY PREDICTION", page_icon=":material/model_training:", layout="wide")
+
+# columns
+left_col, main_col = st.columns([4, 10])
+
+# For user input
+x_test = []
+
+# Left for wine variables
+with left_col:
+    st.header("Wine Variables")
+    
+    # Wrap the inputs in a scrollable container
+    st.markdown("""
+        <div style="height: 80%; overflow-y: auto; padding-right: 10px;">
+    """, unsafe_allow_html=True)
+
+    fixed_acidity = st.number_input("Fixed Acidity", min_value=0.0, step=0.1)
+    volatile_acidity = st.number_input("Volatile Acidity", min_value=0.0, max_value=1.0, step=0.1)
+    citric_acid = st.number_input("Citric Acid", min_value=0.0, max_value=1.0, step=0.1)
+    residual_sugar = st.number_input("Residual Sugar", min_value=0.0, step=0.1)
+    chlorides = st.number_input("Chlorides", min_value=0.000, max_value=1.000, step=0.001, format="%.3f")
+    free_sulfur_dioxide = st.number_input("Free Sulfur Dioxide", min_value=0.0, step=0.1)
+    total_sulfur_dioxide = st.number_input("Total Sulfur Dioxide", min_value=0.0, step=0.1)
+    density = st.number_input("Density", min_value=0.0, max_value=1.0, step=0.1, format="%.4f")
+    ph = st.number_input("pH", min_value=0.0, max_value=14.0, step=0.1)
+    sulphates = st.number_input("Sulphates", min_value=0.0, step=0.1, format="%.3f")
+    alcohol = st.number_input("Alcohol", min_value=0.0, step=0.1)
+    
+    x_test.extend([fixed_acidity, volatile_acidity, citric_acid, residual_sugar, chlorides, free_sulfur_dioxide, total_sulfur_dioxide, density, ph, sulphates, alcohol])
+    st.markdown("</div>", unsafe_allow_html=True) 
+
+# Right for prediction
+with main_col:
+    st.header("RED WINE QUALITY PREDICTION")
+    
+    if st.button("Predict", key="predict", on_click=None):
+        # prediction = random.choice([5, 6, 7, 4, 8, 3])
+        prediction_quality = prediction(np.array(x_test).reshape(1, -1))
+        st.subheader(f"The predicted Wine quality is: {prediction_quality}")
+
+    # To add space 
+    st.write("")
+    st.markdown("## How it works")
+    st.markdown("""
+    The objective of this project is to predict the quality of the red variants of the Portuguese wine "Vinho Verde" 
+    using physicochemical and sensory variables. The user enters the correct characteristics of the Portuguese wine 
+    "Vinho Verde" and makes a prediction.
+    """)
+ # Add a white line
+    st.markdown("""
+        <hr style="border:1px solid white; width: 100%; margin-top: 40px;">
+    """, unsafe_allow_html=True)
+
+    # Add logos and release by
+    st.markdown("""
+        <div style="text-align: end; font-size: 16px; color: white;">
+            Released by : 
+            <a href="https://www.linkedin.com/in/omer-alt-fotso-992624244/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="20px"></a>
+            <a href="https://www.linkedin.com/in/miorar/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="20px"></a>
+            <a href="https://www.linkedin.com/in/profile3/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="20px"></a>
+            <a href="https://github.com/yourgithub/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="20px"></a>
+        </div>
+    """, unsafe_allow_html=True)
+
+# Customize the text size and other styles
+st.markdown("""
+    <style>
+    /* Set text size for all text elements */
+    .css-1v0mbdj p, .css-1v0mbdj h2, .css-1v0mbdj h1, .css-1v0mbdj h3, .css-1v0mbdj li {
+        font-size: 16px !important;
+    }
+
+    .stButton button {
+        color: white;
+    }
+    .stButton button:hover {
+        background-color: #6cd625;
+    }
+    /*.css-18e3th9 {
+        padding-top: 0rem;
+        padding-bottom: 0rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }*/
+    .block-container {
+        padding-bottom: 1rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        width: 100%;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+# CSS for the Predict button
+# st.markdown("""
+#     <style>
+#     .stButton button {
+#         background-color: #6cd625;
+#         color: white;
+#     }
+#     .stButton button:hover {
+#         background-color: #6cd625;
+#     }
+#     </style>
+# """, unsafe_allow_html=True)
